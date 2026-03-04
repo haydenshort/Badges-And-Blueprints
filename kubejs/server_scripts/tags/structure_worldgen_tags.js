5f84966e3d67a4773096e941346f2f1df1856281
hayden_short <167613220+haydenshort@users.noreply.github.com>
2026-03-03 23:03:27 -0700

    Delete kubejs directory

--- kubejs/server_scripts/tags/structure_worldgen_tags.js
+++
@@ -1,5 +0,0 @@
-console.info("[BAB] StructureTags.js loaded")
-
-ServerEvents.tags("worldgen/structure", event => {
-  event.add("cataclysm:berserker_spawn", "betterfortresses:fortress")
-})
--- kubejs/server_scripts/tweaks/disable_loot_tables.js
+++
@@ -1,9 +0,0 @@
-console.info("[BAB] DisableLootTables.js loaded")
-
-ServerEvents.generateData('after_mods', event => {
-    let ids = JsonIO.read("kubejs/server_scripts/tweaks/disable_loot_tables.json").ids
-    
-    for (let id of ids) {
-        event.json(`${id}.json`, {"neoforge:conditions": [{"type": "neoforge:false"}]})
-    }
-})
